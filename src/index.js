e0cea8d2b4587ea5726d49e0e73209dc57f0c35d
Varvara Gaponova <gaponova.varvara21091995@gmail.com>
2023-01-22 02:31:18 +0400

    feat: finished task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,42 @@
 
 // You should implement your task here.
+// Option 1
+// module.exports = function towelSort (matrix) {
+//   return matrix ? matrix.map((item, index) => {
+//     if(index % 2 !== 0) {
+//       return item.reverse();
+//     }
+//     return item;
+//   }).flat(Infinity) : [];
+// }
 
+
+//Option 2
+// module.exports = function towelSort (matrix) {
+//   if (!matrix) return [];
+
+//   const items = [];
+
+//   matrix.forEach((item, index) => {
+//     if(index % 2 !== 0) {
+//       items.push(...item.reverse());
+//     }
+//     else items.push(...item);
+//   });
+
+//   return items;
+// }
+
+//Option 3
 module.exports = function towelSort (matrix) {
-  return [];
+  if (!matrix) return [];
+
+  return matrix.reduce((acc, item, index) => {
+    if(index % 2 !== 0) {
+      acc.push(...item.reverse());
+    } else {
+      acc.push(...item);
+    }
+      return acc;
+  }, [])
 }
